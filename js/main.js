845b2b2f818830017971db84a8494d4355c3e4ad
Bressan1 <edu.nick7@gmail.com>
2026-02-23 21:26:09 -0300

    creat function and alert

--- js/main.js
+++ js/main.js
@@ -2,3 +2,15 @@ const relogio = document.querySelector('.relogio');
 const iniciar = document.querySelector('.iniciar');
 const pausar = document.querySelector('.pausar');
 const zerar = document.querySelector('.zerar');
+
+iniciar.addEventListener('click', function(event){
+    alert('Iniciou');
+})
+
+pausar.addEventListener('click', function(event){
+    alert('Pausou');
+})
+
+zerar.addEventListener('click', function(event){
+    alert('Zerou');
+})

def7197374fc9f125f834ff2d5bf255b3f2cb4d1
Bressan1 <edu.nick7@gmail.com>
2026-02-24 22:16:42 -0300

    configured the timer, certi the timer cameback a 0 again when stop or inicial again

--- js/main.js
+++ js/main.js
@@ -1,16 +1,40 @@
+
+function setTimeSecund(segundos){
+    const data = new data(segundos * 1000);
+    return data.toLocaleTimeString('pt-BR', {
+        hour12: false,
+        timeZone: 'GMT'
+    });
+}
+
+
 const relogio = document.querySelector('.relogio');
 const iniciar = document.querySelector('.iniciar');
 const pausar = document.querySelector('.pausar');
 const zerar = document.querySelector('.zerar');
 
+let segundos = 0;
+let timer;
+
+function iniciaRelogio(){
+    timer =setInterval(function(){
+        segundos++;
+        relogio.innerHTML = setTimeSecund(segundos);
+    }, 1000);
+}
+
+
 iniciar.addEventListener('click', function(event){
-    alert('Iniciou');
+    clearInterval(timer);
+    iniciaRelogio();
 })
 
 pausar.addEventListener('click', function(event){
-    alert('Pausou');
+    clearInterval(timer);
 })
 
 zerar.addEventListener('click', function(event){
-    alert('Zerou');
+   clearInterval(timer); 
+   relogio.innerHTML = '00:00:00';
+   segundos = 0;
 })

f7802816860aff9847a66ec023ec8d318cba1a80
Bressan1 <edu.nick7@gmail.com>
2026-02-24 22:40:24 -0300

    finish the timer, using clean code, good pratics

--- js/main.js
+++ js/main.js
@@ -1,40 +1,63 @@
-
-function setTimeSecund(segundos){
-    const data = new data(segundos * 1000);
+function relogio() {
+  function setTimeSecund(segundos) {
+    const data = new Date(segundos * 1000);
     return data.toLocaleTimeString('pt-BR', {
-        hour12: false,
-        timeZone: 'GMT'
+      hour12: false,
+      timeZone: 'GMT',
     });
-}
+  }
 
+  const relogio = document.querySelector('.timer');
 
-const relogio = document.querySelector('.relogio');
-const iniciar = document.querySelector('.iniciar');
-const pausar = document.querySelector('.pausar');
-const zerar = document.querySelector('.zerar');
+  let segundos = 0;
+  let timer;
 
-let segundos = 0;
-let timer;
-
-function iniciaRelogio(){
-    timer =setInterval(function(){
-        segundos++;
-        relogio.innerHTML = setTimeSecund(segundos);
+  function iniciaRelogio() {
+    timer = setInterval(function () {
+      segundos++;
+      relogio.innerHTML = setTimeSecund(segundos);
     }, 1000);
+  }
+
+  document.addEventListener('click', function (e) {
+    const el = e.target;
+
+    if (el.classList.contains('iniciar')) {
+      relogio.classList.remove('pausado');
+      clearInterval(timer);
+      iniciaRelogio();
+    }
+
+    if (el.classList.contains('pausar')) {
+      clearInterval(timer);
+      relogio.classList.add('pausado');
+    }
+
+    if (el.classList.contains('zerar')) {
+      clearInterval(timer);
+      relogio.innerHTML = '00:00:00';
+      segundos = 0;
+      relogio.classList.remove('pausado');
+    }
+  });
 }
 
+relogio();
 
-iniciar.addEventListener('click', function(event){
-    clearInterval(timer);
-    iniciaRelogio();
-})
 
-pausar.addEventListener('click', function(event){
-    clearInterval(timer);
-})
+// iniciar.addEventListener('click', function(event){
+//     relogio.classList.remove('pausado');
+//     clearInterval(timer);
+//     iniciaRelogio();
+// })
 
-zerar.addEventListener('click', function(event){
-   clearInterval(timer); 
-   relogio.innerHTML = '00:00:00';
-   segundos = 0;
-})
+// pausar.addEventListener('click', function(event){
+//     relogio.classList.add('pausado');
+//     clearInterval(timer);
+// })
+
+// zerar.addEventListener('click', function(event){
+//    clearInterval(timer); 
+//    relogio.innerHTML = '00:00:00';
+//    segundos = 0;
+// })
